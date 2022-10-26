e2d815dc17b4c887464e5a008e48e77f58c61568
Italo Colt <103470585+Coltinho@users.noreply.github.com>
2022-10-26 16:21:07 -0300

    up

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,6 +1,15 @@
  import "./MyForm.css"
-
+ import {useState} from 'react'
+ 
 const MyForm = () => {
+  //3 - gerencmaneto de dados
+  const [name,setName] = useState()
+  const [email,setEmail] = useState()
+
+  const handlename = (e) => {
+    console.log("mudou o nome");
+  }
+
   return (
     <div>
         {/* 1 - criação de form*/}
@@ -8,9 +17,13 @@ const MyForm = () => {
 
     <div>    
         <label htmlFor='name'>Nome</label>
-        <input type="text" name="name" placeholder="Digite o seu nome"/>
-
+        <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleChange}/>
     </div>
+    {/* 2 - label envolvendo input */}
+    <label>
+      <span>Email:</span>
+      <input type="email" name="email" placeholder="Digite o seu e-mail"/>
+    </label>
     <input type= "submit" value="enviar"/>
     </form>
     </div>
