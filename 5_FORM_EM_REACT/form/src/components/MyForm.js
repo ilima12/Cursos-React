4ab0a48c9baf87e81d396d9e38dc02e0ea58cd9f
Italo Colt <103470585+Coltinho@users.noreply.github.com>
2022-10-27 00:10:22 -0300

    up

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -7,22 +7,35 @@ const MyForm = () => {
   const [email,setEmail] = useState()
 
   const handlename = (e) => {
-    console.log("mudou o nome");
-  }
+    setName(e.target.value);
+  };
+  console.log(name);
 
+  const handlemail= (e) => {
+    setEmail(e.target.value)
+  };
+  console.log(email);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("enviando o formulário");
+    console.log(name,email);
+  }
   return (
     <div>
+      {/*5 - envio de form*/}
         {/* 1 - criação de form*/}
-    <form>
+    <form onSubmit={handleSubmit}>
 
     <div>    
         <label htmlFor='name'>Nome</label>
-        <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleChange}/>
+        <input type="text" name="name" placeholder="Digite o seu nome" onChange={handlename}/>
     </div>
     {/* 2 - label envolvendo input */}
     <label>
       <span>Email:</span>
-      <input type="email" name="email" placeholder="Digite o seu e-mail"/>
+      {/*simplificação de manupulação de states*/}
+      <input type="email" name="email" placeholder="Digite o seu e-mail" onChange={handlemail}/>
     </label>
     <input type= "submit" value="enviar"/>
     </form>
