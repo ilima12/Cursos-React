eae822da141c04505c6729eabb097b495cf28067
Italo Colt <103470585+Coltinho@users.noreply.github.com>
2022-11-07 22:41:55 -0300

    update

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -24,12 +24,25 @@ function App() {
   const[gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordslist);
 
-  console.log(words);
+  //start the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  //process the letter input
+  const verifyLetter = () =>{
+    setGameStage(stages[2].name);
+  }
+
+  //restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen/>}
-      {gameStage === "game" && <Game/>}
-      {gameStage === "end" && <GameOver/>}
+      {gameStage === "start" && <StartScreen startGame={startGame}/>}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === "end" && <GameOver retry={retry}/>}
     </div>
   );
 }
--- 6_SECRET_WORD/secretword/src/components/Game.js
+++ 6_SECRET_WORD/secretword/src/components/Game.js
@@ -1,8 +1,13 @@
 import "./Game.css"
 
-const Game = () => {
+const Game = ({verifyLetter}) => {
   return (
-    <div>Game</div>
+    <div>
+      
+      <h1>Game</h1>
+      <button onClick={verifyLetter}>Finalizar jogo</button>
+    
+    </div>
   )
 }
 
