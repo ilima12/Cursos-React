df8887629ec6095590ce1f836076721913a67936
Italo Colt <103470585+ilima12@users.noreply.github.com>
2022-11-08 22:12:33 -0300

    update

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -24,8 +24,42 @@ function App() {
   const[gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordslist);
 
+  const[pickedWord, setPickedWord] = useState("");
+  const[pickedCategory, setPickedCategory] = useState("");
+  const[letters, setLetters] = useState([]);
+
+  const pickWordCategory = () => {
+  //pick a random category  
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+    
+    //pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    console.log (word);
+
+    return {word, category};
+  }
+
   //start the secret word game
   const startGame = () => {
+    //pick word and pick category
+    const {word, category} = pickWordCategory();
+    
+    //create an array of letters
+    let wordLetters = word.split("");
+    
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log (word, category);
+    console.log (wordLetters);
+
+    //fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   };
 
