644eb73051937c3c9f3b542fa8856cf0b1a4cf60
Italo Colt <103470585+ilima12@users.noreply.github.com>
2022-11-09 10:49:19 -0300

    update

--- 6_SECRET_WORD/secretword/src/components/Game.js
+++ 6_SECRET_WORD/secretword/src/components/Game.js
@@ -2,13 +2,36 @@ import "./Game.css"
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar jogo</button>
-    
+    <div className="game">
+      <p className="points">
+      <span> ponteação: 000 </span>
+      </p>
+      <h1>
+        Advinhe a palavra
+      </h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <p>Você ainda tem XXX tentativas(s).</p> 
+      <div className="wordContainer">
+          <span className="letter">A</span>
+          <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+      <p>tente adivinhar uma letra da palavra:</p>
+      <form>
+        <input type="text" name="letter" maxLength="1" required/>
+        <button>Jogar!</button>
+      </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a, </span>
+        <span>b, </span>
+
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Game
