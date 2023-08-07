311ceb6ab88b84b64bd9483d91c654dbee8a24f6
Italo Lima <103470585+ilima12@users.noreply.github.com>
2023-08-07 18:41:44 -0300

    restaurando Post 5:13

--- 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
+++ 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
@@ -3,6 +3,10 @@ import {useState, useEffect} from 'react'
 export const useFetch = (url) => {
     const [data, setData] = useState(null);
 
+// 5 - refatorando post
+const [config, setConfig] = useState(null);
+const [method, setMethod] = useState(null);
+const [callFetch, setCallFetch] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -15,7 +19,23 @@ export const useFetch = (url) => {
      
        fetchData();
         
-    },[url]);
+    },[url, callFetch]);
+
+// 5 - refatorando POST
+const httpRequest = async () => {
+    if (method === "POST") {
+        let fetchOptions = [url, config];
+    
+        const res = await fetch(...fetchOptions);
+    
+        const json = await res.json();
+    
+        setCallFetch(json);
+}
+}
+
+    httpRequest();
+}, [config]);
 
     return { data };
 };
